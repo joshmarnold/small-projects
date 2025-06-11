9507260cfa031583dc726b0c3a5394d550452fed
jarnld2@gmail.com <jarnld2@gmail.com>
2025-06-11 11:41:41 -0400

    fix mobile nav

--- colorfusion/src/components/Header.tsx
+++ colorfusion/src/components/Header.tsx
@@ -104,7 +104,7 @@ export const Header = () => {
               duration: 0.8,
               ease: "easeOut",
             }}
-            className="hidden h-10 min-w-[120px] rounded-md bg-orange-500 px-4 py-2 font-medium text-white transition-colors hover:bg-orange-500/90 md:flex"
+            className="hidden h-10 min-w-[120px] rounded-md bg-orange-500 px-4 py-2 font-medium text-white transition-colors hover:bg-orange-500/90 md:flex text-nowrap"
           >
             Get Started
           </motion.button>
